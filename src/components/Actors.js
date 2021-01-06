6de4a4b4d3ab63494f276bf327e6c56b07e70532
Dice-K39 <daisukehashimoto39@gmail.com>
2021-01-05 22:39:48 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,6 +5,29 @@ const Actors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      <br />
+      
+      {actors.map((actor, id) =>
+      {
+        return (
+          <div key={id}>
+            Name: {actor.name}
+            <br />
+            Movies:
+            <ul>
+              {actor.movies.map((movie, id) => 
+              {
+                return (
+                  <li key={id}>
+                    {movie}
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
+const Movies = () => 
+{
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, id) => 
+        {
+          return (
+            <div key={id}>
+              Title: {movie.title}
+              <br />
+              Play Time: {movie.time}
+              <br />
+              Genres:
+              {movie.genres.map((genre, id) => 
+              {
+                return (
+                  <li key={id}>
+                    {genre}
+                  </li>
+                )
+              })}
+              <br />
+            </div>
+          )
+        })}
     </div>
   );
 };

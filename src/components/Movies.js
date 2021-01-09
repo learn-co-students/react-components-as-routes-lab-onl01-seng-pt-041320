616c0153d41abc4418793be762771d8572cc74c1
christinezosche <christine.zosche@gmail.com>
2021-01-09 09:35:03 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  
   return (
+    
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      
+      {movies.map((movie) => {
+        return (<div>
+        <h1>{movie.title}</h1>
+        <h1>{movie.time}</h1>
+        <ul>{movie.genres.map((genre) => <li>{genre}</li>)}
+        </ul>
+        </div>)}
+        )}
     </div>
   );
 };

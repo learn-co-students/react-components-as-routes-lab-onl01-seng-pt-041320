f4d23385dd7bdddf0b1f8b30aa18c974008d384b
baker615512 <baker615512@gmail.com>
2021-01-09 21:21:44 -0600

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,19 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director) => (
+        <div>
+          <h3>{director.name}</h3>
+          <p>Movies:
+            <ul>
+              {director.movies.map((movie) => (
+                <li>{movie}</li>
+              ))}
+            </ul>
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) => (
+          <div>
+            <h3>{movie.title}</h3>
+            <p>{movie.time}</p>
+            <p>Genres:
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li>{genre}</li>
+                ))}
+              </ul>
+            </p>
+          </div>
+        ))}
     </div>
   );
 };

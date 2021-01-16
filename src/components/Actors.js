a6c478b72ac09e5b2e98dd7d2663d371a8914aa3
chica25 <patricia@patriciaco.com>
2021-01-15 22:02:17 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,10 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+        {/* {actors.map((actor, index) => (key={index} {actor} */}
     </div>
-  );
-};
+  )
+}
 
 export default Actors;
